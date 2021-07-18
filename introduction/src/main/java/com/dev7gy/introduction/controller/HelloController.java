d428d2b3a692a5519e3e10a333ba740043711927
dev7gy <fun7gy125@gmail.com>
2021-07-18 23:47:26 +0900

    thymleaf 템플릿을 통한 Controller 및 ResponseBody 사용 예제

--- introduction/src/main/java/com/dev7gy/introduction/controller/HelloController.java
+++ introduction/src/main/java/com/dev7gy/introduction/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.dev7gy.introduction.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -20,5 +22,41 @@ public class HelloController {
     {
         model.addAttribute("data", "hello! introduction");
         return "hello";
+    }
+
+    @GetMapping("mvc")
+    public String mvc(@RequestParam(required = false, value = "name") String name, Model model) {
+        if (name.isEmpty()) {
+            name = "empty";
+        }
+        model.addAttribute("name", name);
+        return "mvc-template";
+        /** viewResolver를 통해서 mvc-template.html 파일을 찾음
+         *
+         */
+    }
+
+    @GetMapping("api")
+    @ResponseBody
+    public Api responseBody(@RequestParam(required = false, value = "name") String name) {
+        Api api = new Api();
+        api.setName(name);
+        return api;
+        /** viewResolver 대신 HttpMessageConverter가 동작
+         * http://localhost:8080/api?name=test
+         * {"name":"test"}
+         */
+    }
+
+    static class Api {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
