18c1c6c9e792a9d936eed8c7eda6d008165da139
dev7gy <fun7gy125@gmail.com>
2021-08-11 23:06:47 +0900

    introduction - Repository 도 test코드  추가

---
+++ introduction/src/main/java/com/dev7gy/introduction/Repository/MemoryModelRepository.java
@@ -0,0 +1,39 @@
+package com.dev7gy.introduction.repository;
+
+import com.dev7gy.introduction.model.Model;
+
+import java.util.*;
+
+public class MemoryModelRepository implements ModelRepository {
+    private static Map<Long, Model> store = new HashMap<>();
+    private static long sequence = 0L;
+    @Override
+    public Model save(Model model) {
+        model.setId(++sequence);
+        store.put(model.getId(), model);
+        return model;
+    }
+
+    @Override
+    public Optional<Model> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Model> findByName(String name) {
+        return store.values().stream()
+                .filter(model -> model.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Model> findAll() {
+        Collection<Model> values = store.values();
+
+        return new ArrayList<Model>(values);
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
---
+++ introduction/src/test/java/com/dev7gy/introduction/repository/MemoryModelRepositoryTest.java
@@ -0,0 +1,58 @@
+package com.dev7gy.introduction.repository;
+
+import com.dev7gy.introduction.model.Model;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MemoryModelRepositoryTest {
+    MemoryModelRepository repository = new MemoryModelRepository();
+
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    @Test
+    public void save() {
+        //given
+        Model model = new Model();
+        model.setName("spring");
+        //when
+        repository.save(model);
+        //then
+        Model result = repository.findById(model.getId()).get();
+        assertThat(model).isEqualTo(result);
+    }
+    @Test
+    public void findByName() {
+        //given
+        Model model1 = new Model();
+        model1.setName("spring1");
+        repository.save(model1);
+        Model model2 = new Model();
+        model2.setName("spring2");
+        repository.save(model2);
+        //when
+        Model result = repository.findByName("spring1").get();
+        //then
+        assertThat(result).isEqualTo(model1);
+    }
+    @Test
+    public void findAll() {
+        //given
+        Model model1 = new Model();
+        model1.setName("spring1");
+        repository.save(model1);
+        Model model2 = new Model();
+        model2.setName("spring2");
+        repository.save(model2);
+        //when
+        List<Model> result = repository.findAll();
+        //then
+        assertThat(result.size()).isEqualTo(2);
+    }
+}
