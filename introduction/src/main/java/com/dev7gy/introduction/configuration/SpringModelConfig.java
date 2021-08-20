1ef63ea9f0a6514c111d66f37a1a433845530a43
dev7gy <fun7gy125@gmail.com>
2021-08-21 01:13:53 +0900

    Delete @Repository annotation, then using Datasource

--- introduction/src/main/java/com/dev7gy/introduction/configuration/SpringModelConfig.java
+++ introduction/src/main/java/com/dev7gy/introduction/configuration/SpringModelConfig.java
@@ -1,15 +1,24 @@
 package com.dev7gy.introduction.configuration;
 
+import com.dev7gy.introduction.repository.JdbcSpringModelRepository;
 import com.dev7gy.introduction.repository.MemorySpringModelRepository;
 import com.dev7gy.introduction.repository.SpringModelRepository;
 import com.dev7gy.introduction.service.SpringModelService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
 /**
  * 자바 코드로 직접 스프링 빈 등록하기
  */
 @Configuration
 public class SpringModelConfig {
+    private final DataSource dataSource;
+
+    public SpringModelConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public SpringModelService springModelService() {
         return new SpringModelService(springModelRepository());
@@ -17,6 +26,7 @@ public class SpringModelConfig {
 
     @Bean
     public SpringModelRepository springModelRepository() {
-        return new MemorySpringModelRepository();
+        // return new MemorySpringModelRepository();
+        return new JdbcSpringModelRepository(dataSource);
     }
 }
