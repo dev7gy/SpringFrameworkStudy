93ee82941cd9e40ca2c78ae8f49cdb15e9ae8895
dev7gy <fun7gy125@gmail.com>
2021-09-16 18:10:41 +0900

    Add AutoConfig test #core

---
+++ core/src/test/java/com/dev7gy/core/AutoAppConfigTest.java
@@ -0,0 +1,19 @@
+package com.dev7gy.core;
+
+import com.dev7gy.core.member.Member;
+import com.dev7gy.core.member.service.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class AutoAppConfigTest {
+    @Test
+    void basicScan() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
