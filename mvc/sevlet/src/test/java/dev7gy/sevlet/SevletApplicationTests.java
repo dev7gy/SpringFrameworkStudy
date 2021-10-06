56f9a82f4b7bc78aa6370d67ec33d5d4a543022f
dev7gy <fun7gy125@gmail.com>
2021-10-06 22:06:25 +0900

    start mvc-servlet #mvc1

---
+++ mvc/sevlet/src/test/java/dev7gy/sevlet/SevletApplicationTests.java
@@ -0,0 +1,13 @@
+package dev7gy.sevlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
